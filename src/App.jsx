cb92480939f076a7b1fab35c7dcd0bc4e1c64913
Lougerb <glouien@gmail.com>
2024-05-21 14:03:00 +0800

    casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 
 // Layout
-import Nav from './project/layout/Nav/Nav';
-import Footer from './project/layout/Footer/Footer';
-import Header from './project/layout/Header/Header';
+import Nav from './Project/Layout/Nav/Nav';
+import Footer from './Project/Layout/Footer/Footer';
+import Header from './Project/Layout/Header/Header';
 
 // Content
-import Home from './project/pages/Home/Home';
-import About from './project/pages/About/About';
-import Product from './project/pages/Product/Product';
-import Services from './project/pages/Services/Services';
-import Contact from './project/pages/Contact/Contact';
-import NotFound from './project/pages/NotFound/NotFound';
+import Home from './Project/Pages/Home/Home';
+import About from './Project/Pages/About/About';
+import Product from './Project/Pages/Product/Product';
+import Services from './Project/Pages/Services/Services';
+import Contact from './Project/Pages/Contact/Contact';
+import NotFound from './Project/Pages/NotFound/NotFound';
 
 // Packages
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
