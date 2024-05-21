7c9dc398ea84428b06605d43a236afe697b1bf57
Lougerb <glouien@gmail.com>
2024-05-21 13:08:13 +0800

    qw

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			src: "/src",
-			'@Project': path.resolve(__dirname, './src/Project'),
+			'@Project': path.resolve(__dirname, './src/tite'),
 			'@Assets': path.resolve(__dirname, './src/assets'),
 		},
 	},
